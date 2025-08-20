9c403c3b0dd54e9f35817c5b8e3c710986516441
nitin <nitinkhare207@gmail.com>
2025-08-20 23:10:55 +0530

    javascript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//   let score=undefined
+   
+//   console.log(typeof score)
+
+//   let valueInNumber=Number(score)
+//     console.log(typeof valueInNumber) // number
+//     console.log(valueInNumber);
+    
+//"33" => 33
+// "33abc" => NaN
+// true => 1 , // false => 0
+// null => 0
+// undefined => NaN
+
+let Ans=1
+let booleanIsAns=Boolean(Ans)
+console.log(booleanIsAns); // true
+
+//1 => true; // 0 => false
+// "" => false; 
+// "nitin" => true;
+
+let someNum=33
+let stringNum=String(someNum)
+console.log(stringNum); // "33"
+console.log(typeof stringNum); // string
