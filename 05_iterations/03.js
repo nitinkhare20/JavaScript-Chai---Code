43638fb7d9daa88474fa825b076669cb5dda9f7a
nitin <nitinkhare207@gmail.com>
2025-08-27 21:50:59 +0530

    Loops

---
+++ 05_iterations/03.js
@@ -0,0 +1,45 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr =[1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings = "Hello World!"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+    
+
+//  Maps
+const map = new Map() 
+map.set('IN',"India")
+map.set('USA', "United States of America")
+map.set('FR',"France")
+map.set('IN',"India")
+// console.log(map);
+
+
+
+// for (const [key,value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+
+const myobj ={
+    'game1': 'Cricket',
+    'game2': 'Football',
+}
+
+// for (const [key,value] of myobj) {
+//      console.log(key, ':-', value);
+// }
+
+
+
