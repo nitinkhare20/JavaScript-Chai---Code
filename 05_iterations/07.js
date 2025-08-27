b72ac551ddc6c7a037be9c743acb7faa97f83e14
nitin <nitinkhare207@gmail.com>
2025-08-27 23:13:34 +0530

    filter

---
+++ 05_iterations/07.js
@@ -0,0 +1,10 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10}) // [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
